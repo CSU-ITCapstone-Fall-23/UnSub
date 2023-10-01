70b66d41890034016560633bfb945ca110e00207
evanonb <owenby_evan@students.columbusstate.edu>
2023-10-01 23:51:54 +0000

    implemented Google API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request           # import flask framework
+'''from flask import Flask, render_template, request           # import flask framework
 app = Flask(__name__)                                       # create an app instance
 
 import urllib.request, json
@@ -25,4 +25,49 @@ def about():                                                # method called abou
 
 
 if __name__ == "__main__":                                  # when running python app.py
-    app.run(debug=True)                                     # run the flask app
+    app.run(debug=True)                                     # run the flask app'''
+
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.subscriptions.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+    if os.path.exists(CLIENT_SECRETS_FILE):
+        print('yes, the client secrets file exists')
+    else:
+        print('the client secrets file does not exist!')
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.subscriptions().list(
+        part="subscriberSnippet",
+        mine=True
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
